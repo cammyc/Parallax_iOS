f8a811391fd5cba8539cc86a9e50f57c94574419
cammyc <camconnor0903@gmail.com>
2018-11-25 16:17:59 -0700

    Initial Commit

---
+++ Parallax/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parallax
+//
+//  Created by Cameron Connor on 11/25/18.
+//  Copyright © 2018 Parallax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ParallaxTests/ParallaxTests.swift
@@ -0,0 +1,34 @@
+//
+//  ParallaxTests.swift
+//  ParallaxTests
+//
+//  Created by Cameron Connor on 11/25/18.
+//  Copyright © 2018 Parallax. All rights reserved.
+//
+
+import XCTest
+@testable import Parallax
+
+class ParallaxTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
